901680d3d91c9e59cd52951fb6e1baf552075813
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2015-12-20 13:28:53 +0530

    modify eval script

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -16,14 +16,14 @@ mkdir -p $workspace_dir/{evaluation,log}
 
 cp $1 $workspace_dir/train.conf
 
-# unsupervised 
-# test
-time python $XLIT_HOME/src/cfilt/transliteration/transliterate_cli.py transliterate_topn \
-    $model_fname $lm_fname $test_fcorpus_fname $workspace_dir/evaluation/test.nbest.$tgt_lang 10 $decoder_params_fname > $workspace_dir/log/decode.log 2>&1 
-
-# convert to 1-best format 
-python $XLIT_HOME/src/cfilt/transliteration/news2015_utilities.py  convert_to_1best_format  \
-    $workspace_dir/evaluation/test.nbest.$tgt_lang $workspace_dir/evaluation/test.$tgt_lang 
+## unsupervised 
+## test
+#time python $XLIT_HOME/src/cfilt/transliteration/transliterate_cli.py transliterate_topn \
+#    $model_fname $lm_fname $test_fcorpus_fname $workspace_dir/evaluation/test.nbest.$tgt_lang 10 $decoder_params_fname > $workspace_dir/log/decode.log 2>&1 
+#
+## convert to 1-best format 
+#python $XLIT_HOME/src/cfilt/transliteration/news2015_utilities.py  convert_to_1best_format  \
+#    $workspace_dir/evaluation/test.nbest.$tgt_lang $workspace_dir/evaluation/test.$tgt_lang 
 
 # generate NEWS 2015 evaluation format output file 
 python $XLIT_HOME/src/cfilt/transliteration/news2015_utilities.py gen_news_output \
