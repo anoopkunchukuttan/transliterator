7854efd017e8d64ee675c1ac61b4aefaf9e06964
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2016-08-09 08:37:18 +0530

    add license info to all files

--- src/cfilt/transliteration/parallel_decoder.py
+++ src/cfilt/transliteration/parallel_decoder.py
@@ -1,3 +1,20 @@
+#Copyright Anoop Kunchukuttan 2015 - present
+# 
+#This file is part of the IITB Unsupervised Transliterator 
+#
+#IITB Unsupervised Transliterator is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#IITB Unsupervised Transliterator  is distributed in the hope that it will be useful, 
+#but WITHOUT ANY WARRANTY; without even the implied warranty of 
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+#GNU General Public License for more details. 
+#
+#You should have received a copy of the GNU General Public License 
+#along with IITB Unsupervised Transliterator.   If not, see <http://www.gnu.org/licenses/>.
+
 from multiprocessing import Pool
 import multiprocessing
 import functools
